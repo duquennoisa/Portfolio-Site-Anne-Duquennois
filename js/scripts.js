d9ea5f09912fb8095278b4bd345832cb9b1483f9
duquennoisa <70037329+duquennoisa@users.noreply.github.com>
2021-03-29 18:03:11 -0600

    2.1 intro to javascripts
    included some test js

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+console.log("Welcome to my Portfolio!");
+
+var age = 33;
+var name = 'Anne';
+console.log(name);
+console.log(age);
+
+var size = 100;
+var maxSize = (size * 4) + 15;
+var minSize = (size / 3) - 15 + (size * 2);
+console.log(maxSize);
