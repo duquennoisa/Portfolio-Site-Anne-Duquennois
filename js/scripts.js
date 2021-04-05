6b3222e8404695903eb78717191a947ea72688b1
duquennoisa <70037329+duquennoisa@users.noreply.github.com>
2021-04-04 18:04:42 -0600

    2.3 wrap up
    completed mobile hamburger menu and styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,10 @@
 console.log("Welcome to my Portfolio!");
 
-var age = 33;
-var name = 'Anne';
-console.log(name);
-console.log(age);
-
-var size = 100;
-var maxSize = (size * 4) + 15;
-var minSize = (size / 3) - 15 + (size * 2);
-console.log(maxSize);
-
-var myObject = {
-  name: name,
-  age: age,
-  hobbies: "hiking, traveling, eating",
-};
-
-
-console.log(myObject);
-console.log(myObject.hobbies);
+function menuToggle() {
+	var x = document.getElementById('myNavtoggle');
+	 if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
